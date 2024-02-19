2bf5434b3ea29b5759ae3187e56e617cd2cb4203
AbhinasDash <abhinasdash143@gmail.com>
2024-02-19 13:48:44 +0530

    fs.readFile

--- 1.Starting node/app.js
+++ 1.Starting node/app.js
@@ -8,38 +8,46 @@ const server = http.createServer((req, res) => {
   const method = req.method;
 
   if (url === "/") {
-    res.write("<html>");
-    res.write("<head><title>Message</title></head>");
-    res.write(
-      "<body><form action='/msg' method='POST' ><input type='text' name='msg'><button type='submit'>Send</button></form></body>"
-    );
-    res.write("</html>");
-    return res.end();
-  }
-  if (url === "/msg" && method === "POST") {
+    fs.readFile("msg.txt", { encoding: "utf-8" }, (err, data) => {
+      if (err) {
+        console.log(err);
+      }
+      res.write("<html>");
+      res.write("<head><title>Message</title></head>");
+      res.write(
+        `<body><form action='/msg' method='POST' ><input type='text' name='msg'><button type='submit'>Send</button></form><br/><h4>${data}</h4></body>`
+      );
+      res.write("</html>");
+      return res.end();
+    });
+  } else if (url === "/msg" && method === "POST") {
     const data = [];
     req.on("data", (chunk) => {
       data.push(chunk);
       console.log(chunk);
     });
-    req.on("end", () => {
+    return req.on("end", () => {
       const parseData = Buffer.concat(data).toString();
       console.log(parseData);
       const message = parseData.split("=")[1];
-      fs.writeFileSync("msg.txt", message);
+      fs.writeFile("msg.txt", message, (err) => {
+        if (err) {
+          console.error(err);
+          res.writeHead(500, { "Content-Type": "text/plain" });
+          return res.end("Internal Server Error");
+        }
+        res.writeHead(302, { Location: "/" });
+        return res.end();
+      });
     });
-    // res.statusCode = 302;
-    // res.setHeader("Location", "/");
-    res.writeHead(302, { Location: "/" });
-    return res.end();
+  } else {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>404</title></head>");
+    res.write("<body><h2>Not Found</h2></body>");
+    res.write("</html>");
+    res.end();
   }
-
-  res.setHeader("Content-Type", "text/html");
-  res.write("<html>");
-  res.write("<head><title>404</title></head>");
-  res.write("<body><h2>Not Found</h2></body>");
-  res.write("</html>");
-  res.end();
 });
 
 server.listen(4000);
