7b6489f243bc1c89725f7b27a74058f34b6dd605
AbhinasDash <abhinasdash143@gmail.com>
2024-02-19 12:06:50 +0530

    Redirecting request and fs

--- 1.Starting node/app.js
+++ 1.Starting node/app.js
@@ -1,25 +1,35 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-  console.log(req.url, req.headers, req.method);
+  // console.log(req.url, req.headers, req.method);
 
-  if (req.url === "/home") {
-    res.setHeader("Content-Type", "text/plain");
-    res.end(" Welcome to Home Us page");
-  } else if (req.url === "/about") {
-    res.setHeader("Content-Type", "text/plain");
-    res.end(" Welcome to About Us page");
-  } else if (req.url === "/") {
-    res.setHeader("Content-Type", "text/plain");
-    res.end(" Hello World!");
-  } else {
-    res.setHeader("Content-Type", "text/html");
+  const url = req.url;
+  const method = req.method;
+
+  if (url === "/") {
     res.write("<html>");
-    res.write("<head><title>404</title></head>");
-    res.write("<body><h2>Not Found</h2></body>");
+    res.write("<head><title>Message</title></head>");
+    res.write(
+      "<body><form action='/msg' method='POST' ><input type='text' name='msg'><button type='submit'>Send</button></form></body>"
+    );
     res.write("</html>");
-    res.end();
+    return res.end();
   }
+  if (url === "/msg" && method === "POST") {
+    fs.writeFileSync("msg.txt", "Hello World");
+    // res.statusCode = 302;
+    // res.setHeader("Location", "/");
+    res.writeHead(302, { Location: "/" });
+    return res.end();
+  }
+
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>404</title></head>");
+  res.write("<body><h2>Not Found</h2></body>");
+  res.write("</html>");
+  res.end();
 });
 
 server.listen(4000);
